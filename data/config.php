ebbd2e7c45a87ddad18fd27a9169b4ead9ed5ed0
weiki3306 <wen18826478504@163.com>
2016-03-09 00:13:09 +0800

    修改：      data/config.php 	删除：      ecshopdel 	新文件：    temp/compiled/admin/drag.htm.php 	新文件：    temp/compiled/admin/index.htm.php 	新文件：    temp/compiled/admin/menu.htm.php 	新文件：    temp/compiled/admin/start.htm.php 	修改：      temp/query_caches/sqlcache_config_file_92f48e3abde4e013e804e8d8a545a5b0.php 	新文件：    temp/static_caches/api_str.php 	新文件：    temp/static_caches/cloud_remind.php 	新文件：    temp/static_caches/menu_api.php

--- data/config.php
+++ data/config.php
@@ -30,6 +30,6 @@ define('AUTH_KEY', 'this is a key');
 
 define('OLD_AUTH_KEY', '');
 
-define('API_TIME', '2016-03-08 17:19:27');
+define('API_TIME', '2016-03-09 00:08:57');
 
 ?>
--- temp/query_caches/sqlcache_config_file_92f48e3abde4e013e804e8d8a545a5b0.php
+++ temp/query_caches/sqlcache_config_file_92f48e3abde4e013e804e8d8a545a5b0.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1457434268;
+$this->mysql_config_cache_file_time = 1457453335;
 $this->timeline = 0;
 $this->timezone = 0;
 $this->platform = 'OTHER';

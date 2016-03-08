d71814ee06d7fd6b1363aa6296c79467a9d3468d
weiki3306 <wen18826478504@163.com>
2016-03-08 23:52:28 +0800

    添加

---
+++ data/config.php
@@ -0,0 +1,35 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "xg_ecshop";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "weiki";
+
+// table prefix
+$prefix    = "xg_";
+
+$timezone    = "PRC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '2016-03-08 17:19:27');
+
+?>
